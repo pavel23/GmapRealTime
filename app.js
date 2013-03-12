7aee0cf782f5c8e6ff5d7c94874c90d245c99663
Pavel <neo2318@gmail.com>
2013-03-12 17:01:00 -0500

    agregando imagenes y librerias javascript

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'), 
+	routes  = require('./routes'), 
+	http    = require('http'), 
+	path    = require('path'), 
+	cons    = require('consolidate');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', process.env.PORT || 3000);
+app.engine('.html', cons.jade);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+app.use( express.bodyParser() );
+app.use( express.cookieParser() );
+app.use(app.router);
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Cargando GoogleMaps en tiempo real en el puerto " + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+		console.log(socket.id);
+	});
+
 });
